38cd720ca6532b0c1e03fe8c0c653d51b6c3826d
ottagit <ottaosawa@gmail.com>
2024-01-31 12:58:02 +0300

    deploy across regions

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "region_1"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  alias = "region_2"
+}
+
+data "aws_region" "region_1" {
+  provider = aws.region_1
+}
+
+data "aws_region" "region_2" {
+  provider = aws.region_2
+}
+
+terraform {
+  backend "s3" {
+    key = "stage/multi-region/terraform.tfstate"
+    bucket = "batoto-bitange"
+    region = "us-east-1"
+
+    dynamodb_table = "terraone-locks"
+    encrypt = true
+  }
+}
