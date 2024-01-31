943c2bd59a061b6af05c45e5e5aad9259432c007
ottagit <ottaosawa@gmail.com>
2024-01-31 13:04:07 +0300

    apply terraform formatting to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "region_1" {
-  value = data.aws_region.region_1
+  value       = data.aws_region.region_1
   description = "The name of the first region"
 }
 
 output "region_2" {
-  value = data.aws_region.region_2
+  value       = data.aws_region.region_2
   description = "The name of the second region"
 }
